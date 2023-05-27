87889c3ad8b4bb25ff22ad6a3d49c08d8c01f252
unknown <514ftg@gmail.com>
2023-05-27 15:20:06 +0300

    add caсhe in Fast Api

--- Fast_api_hw/src/services/auth.py
+++ Fast_api_hw/src/services/auth.py
@@ -1,3 +1,6 @@
+import redis
+import pickle
+
 from typing import Optional
 
 from jose import JWTError, jwt
@@ -17,6 +20,7 @@ class Auth:
     SECRET_KEY = settings.secret_key
     ALGORITHM = settings.algorithm
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+    r = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
 
     def verify_password(self, plain_password, hashed_password):
         return self.pwd_context.verify(plain_password, hashed_password)
@@ -75,9 +79,15 @@ class Auth:
         except JWTError as e:
             raise credentials_exception
 
-        user = await repository_users.get_user_by_email(email, db)
+        user = self.r.get(f"user:{email}")
         if user is None:
-            raise credentials_exception
+            user = await repository_users.get_user_by_email(email, db)
+            if user is None:
+                raise credentials_exception
+            self.r.set(f"user:{email}", pickle.dumps(user))
+            self.r.expire(f"user:{email}", 900)
+        else:
+            user = pickle.loads(user)
         return user
 
     async def decode_access_token(self, access_token: str):
